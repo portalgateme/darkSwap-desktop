3aa790fd339d83b3619505e7e4c415516b71e62a
Lư Chí Tâm <chitam.lu97@gmail.com>
2026-01-05 11:39:17 +0700

    update config

---
+++ renderer/constants/tokens/hardhat.ts
@@ -0,0 +1,60 @@
+export const hardhatNftTokens = [
+  {
+    name: 'Sablier V2 Lockup Linear NFT',
+    symbol: 'SABV2LL',
+    address: '0xAFb979d9afAd1aD27C5eFf4E27226E3AB9e5dCC9'
+  },
+  {
+    name: 'Sablier V2 Lockup Dynamic NFT',
+    symbol: 'SABV2LD',
+    address: '0x7CC7e125d83A581ff438608490Cc0f7bDff79127'
+  },
+  {
+    name: 'Uniswap V3: Positions NFT',
+    symbol: 'UNIV3POS',
+    address: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88'
+  }
+]
+
+export const hardhatTokens = [
+  {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18,
+    address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    logoURI: '/images/tokens/ETH.png',
+    isTop: true
+  },
+  {
+    name: 'Tether USD',
+    symbol: 'USDT',
+    decimals: 6,
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    logoURI: '/images/tokens/USDT.png',
+    isTop: true
+  },
+  {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    decimals: 6,
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    logoURI: '/images/tokens/USDC.png',
+    isTop: true
+  },
+  {
+    name: 'Wrapped BTC',
+    symbol: 'WBTC',
+    decimals: 8,
+    address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    logoURI: '/images/tokens/WBTC.png',
+    isTop: true
+  },
+  {
+    name: 'Dai Stablecoin',
+    symbol: 'DAI',
+    decimals: 18,
+    address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    logoURI: '/images/tokens/DAI.png',
+    isTop: true
+  }
+]
